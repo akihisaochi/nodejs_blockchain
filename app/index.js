901af53b7d83c356a0ca922f999c18847c184655
akihisaochi <akihisa.ochi.work@gmail.com>
2019-01-16 20:28:14 +0900

    update: create trading api

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 const P2pServer = require('./p2p-server');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
@@ -10,16 +12,30 @@ const app = express();
 app.use(bodyParser.json());
 
 const bc = new Blockchain();
+const wallet = new Wallet();
+const tp = new TransactionPool();
+
 const p2pServer = new P2pServer(bc);
 
 app.get('/blocks', (req, res) => {
   res.json(bc.chain);
 });
+
 app.post('/mine', (req, res) => {
   const block = bc.addBlock(req.body.data);
   console.log(`block added\n${block}`);
   p2pServer.syncChain();
   res.redirect('/blocks');
+});
+
+app.get('/transactions', (req, res) => {
+  res.json(tp.transactions);
+});
+
+app.post('/transact', (req, res) => {
+  const { recipient, amount } = req.body;
+  const transaction = wallet.createTransaction(recipient, amount, tp);
+  res.redirect('/transactions')
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
